1393e9dc24b31f9b75b9caed389ea175d1d68e81
dreamer <bambukovnikolaj@gmail.com>
2023-01-09 13:14:57 +0300

    add setting eslint, connection CodeClimate

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
-    console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + name + '!'); 
-}
-
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${name}!`);
+};
 
 export default welcome;
